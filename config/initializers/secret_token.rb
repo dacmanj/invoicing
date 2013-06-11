56e1995d967f78bc91500af192211610b583e59a
David Manuel <dmanuel@pflag.org>
2013-06-11 16:53:29 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invoicing::Application.config.secret_token = '79b96c1ea990a4020bb036f518e82e41f408883fbed8cb135fe9c8541c746c57dbbdd94c33ab4f511824c588341a11657f36833a5a2d1a6ae23a3c2fb4652926'

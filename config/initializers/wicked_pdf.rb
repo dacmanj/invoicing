5a53f3c05ca78338c4672b40da5ee0505fb1ce5e
David Manuel <dmanuel@pflag.org>
2013-11-20 14:23:12 -0500

    heroku wkhtmltopdf binary

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 WickedPdf.config do |config|  
   if Rails.env == 'production' then
-    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+    config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf-amd64"
   else  ### Following allows for development on my MacBook or Linux box
     if /darwin/ =~ RUBY_PLATFORM then
       config.exe_path = '/usr/local/bin/wkhtmltopdf' 

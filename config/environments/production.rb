7964f2eb5d258a3a0b51d7725855b0d93cd6fd5d
David Manuel <dmanuel@pflag.org>
2013-11-25 17:08:53 -0500

    hide duplicate contact name (for individuals)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,6 @@ Invoicing::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.asset_host = 'https://invoicing-pflag.herokuapp.com'
-  config.action_controller.asset_host = 'https://invoicing-pflag.herokuapp.com'
-  config.action_mailer.asset_host = 'https://invoicing-pflag.herokuapp.com'
-
   config.action_mailer.default :content_type => "text/html"
 
   # ActionMailer Config

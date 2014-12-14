2e92f867f1275dabd61f874415baa0ca25d9aca6
David Manuel <dmanuel@pflag.org>
2014-12-14 10:23:22 -0500

    consolidate authorizations

--- app/authorizers/invoice_authorizer.rb
+++ app/authorizers/invoice_authorizer.rb
@@ -1,21 +1,7 @@
 # app/authorizers/invoice_authorizer.rb
 class InvoiceAuthorizer < ApplicationAuthorizer
 
-  def self.creatable_by?(user)
-    user.has_role? :staff
-  end
-
-  def self.readable_by?(user)
-    user.has_role? :staff or user.has_role? :auditor
-  end
-
-  def self.updatable_by?(user)
-    user.has_role? :staff
-  end
-  def self.deletable_by?(user)
-    user.has_role? :admin
-  end
   def self.emailable_by?(user)
-    user.has_role? :staff
+    user.has_any_role? :staff, :admin
   end
 end

ef95b54f6f9fd2d43cf6395579826ac3dcb9d196
David Manuel <dmanuel@pflag.org>
2015-09-15 16:39:12 -0700

    ignore cert verification for development only

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Invoicing::Application.initialize!
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? 

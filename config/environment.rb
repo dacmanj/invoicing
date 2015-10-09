dfead44e0fa35b58a71100b68eb329b3fb7bef99
David Manuel <dmanuel@pflag.org>
2015-10-09 12:39:54 -0400

    verify peer boot message suppress

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Invoicing::Application.initialize!
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? 
+silence_warnings do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE unless Rails.env.production?
+end
